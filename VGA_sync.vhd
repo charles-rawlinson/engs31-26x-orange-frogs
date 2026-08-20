9b7d148e341219bf77c90fa2ec16d0b8c637ba74
charles-rawlinson <charles.rawlinson.28@dartmouth.edu>
2026-08-20 00:12:56 -0400

    syntax

--- VGA_sync.vhd
+++ VGA_sync.vhd
@@ -36,9 +36,6 @@ port	(
 	vsync_port		: out std_logic;
 	video_on		: out std_logic;
 	hcount			: out std_logic_vector(9 downto 0);
-	vcount			: out std_logic_vector(9 downto 0);
-	video_on		: out std_logic;
-	hcount			: out std_logic_vector(9 downto 0);
 	vcount			: out std_logic_vector(9 downto 0)
 	);
 end vga_sync;
